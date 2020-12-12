def79b54c815fe0404d26748751b3ac759053ecb
monanto <42829957+monanto@users.noreply.github.com>
2020-12-13 03:47:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-apt install git -y
-git clone https://github.com/1tayH/secret.git && cd secret
 docker-compose up --build
 
 
