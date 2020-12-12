3a8d0d5548130424186c66e9eccd38fca57ad0f8
monanto <42829957+monanto@users.noreply.github.com>
2020-12-13 04:03:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 apt install -y docker-compose
+IP=$(curl -s -4 ifconfig.co)
+sed -i "s/localhost/$IP" client/nuxt.config.js
 docker-compose up --build
 
 
